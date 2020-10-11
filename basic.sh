7bb3763bfb5035a7ca4da6bbe771850f45dea3dd
JoeyLipton <harmansohi16@gmail.com>
2020-10-11 17:39:46 -0400

    More updates for the CentOS script

--- basic.sh
+++ basic.sh
@@ -1,9 +1,42 @@
 #!/bin/bash
 
-yum install -y vim wget yum-utils tmux git python38 
 
-echo "alias ls='ls -l'" >> ~/.bashrc
+# Install vim
 
-source .bashrc
+echo "Installing vim"
+yum install -y -q vim 
+
+
+
+# Install wget
+
+echo "Installing wget"
+yum install -y -q wget 
+
+
+# Install yum-utils
+
+echo "Installing yum-utils"
+yum install -y -q yum-utils 
+
+
+# Install tmux
+echo "Installing tmux"
+yum install -y -q tmux 
+
+
+
+# Install git
+echo "Installing git"
+yum install -y -q git 
+
+
+# Install python (and pip)
+echo "Installing python 3.7"
+yum install -y -q python37
+
+
+
+
 
 echo "If you get here there probably wasn't an error"
