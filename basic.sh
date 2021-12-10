28a8a48b13024d618aae785c45d9575463711a55
Harman Sohi <harmansohi16@gmail.com>
2021-12-10 08:55:52 +0000

    Update basic.sh

--- basic.sh
+++ basic.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
 
-
 # Install vim
-
 {
     yum install -y -q vim
     &> /dev/null
     printf "Installed vim\n"
 }
 
-
-
 # Install nano
-
 {
     yum install -y -q nano
     &> /dev/null
@@ -20,50 +15,38 @@
 }
 
 # Install wget
-
 {
     yum install -y -q wget
     &> /dev/null
     printf "Installed wget\n"
 }
 
-
 # Install yum-utils
-
 {
     yum install -y -q yum-utils
     &> /dev/null
     printf "Installed yum-utils\n"
 }
 
-
-
 # Install tmux
-
 {
     yum install -y -q tmux
     &> /dev/null
     printf "Installed tmux\n"
 }
 
-
 # Install git
-
 {
     yum install -y -q git
     &> /dev/null
     printf "Installed git\n"
 }
 
-
 # Install python (and pip)
-
 {
     yum install -y -q python38
     &> /dev/null
     printf "Installed python38\n\n"
 }
-
-
 
 printf "If you get here there probably wasn't an error\n\n"
